bb645794f9b9000aca2e7b9e316aa11d88c2b174
Linus Å <71346418+LynkanRex@users.noreply.github.com>
2021-06-04 10:33:05 +0200

    Setting up 2D arrays, basic game program logic

--- battleships.cpp
+++ battleships.cpp
@@ -4,21 +4,50 @@
 
 #include "battleships.h"
 #include <iostream>
+#include <array>
 using namespace std;
 
 void DoLoop();
-void InitializeGameplayArea();
-void UpdateGameplayArea();
+array<array<int, 10>, 10> InitializeGameplayArea();
+array<array<int, 10>, 10> UpdateGameplayArea(array<array<int, 10>, 10>);
 
-void DoTurn();
+void DoTurn(string);
+bool ValidateInput(string);
+array<char, 2> SplitInput(string);
 
-bool ValidateInput(string input);
-void SplitInput(string input);
 
 
+array<array<int, 10>, 10> InitializeGameplayArea(array<array<int, 10>, 10> currentPlayArea){
+    array<array<int, 10>, 10> playArea;
+
+
+    return playArea;
+}
+
+
+
+void DoTurn(string input){
+    array<char, 2> splitInput = SplitInput(input);
+
+
+}
+
+array<char, 2> SplitInput(string input){
+
+
+}
+
+bool ValidateInput(string input){
+    if(input == "")
+        return 0;
+
+    return 1;
+}
 
 
 void DoLoop(){
+    array<array<int, 10>, 10> playArea = InitializeGameplayArea();
+
     while(true){
         string input = "";
         cin >> input;
@@ -26,6 +55,10 @@ void DoLoop(){
         if(input == "exit"){
             cout << "Thank you for playing!" << endl;
             break;
+        }
+        else{
+            if(ValidateInput(input))
+                DoTurn(input);
         }
     }
 }
