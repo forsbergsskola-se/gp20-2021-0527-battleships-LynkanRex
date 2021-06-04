f669a7c12575ef969095d9ad67144866201ed869
Linus Å <71346418+LynkanRex@users.noreply.github.com>
2021-06-04 10:26:14 +0200

    Initial battleships logic setup

--- battleships.cpp
+++ battleships.cpp
@@ -4,8 +4,33 @@
 
 #include "battleships.h"
 #include <iostream>
+using namespace std;
+
+void DoLoop();
+void InitializeGameplayArea();
+void UpdateGameplayArea();
+
+void DoTurn();
+
+bool ValidateInput(string input);
+void SplitInput(string input);
+
+
+
+
+void DoLoop(){
+    while(true){
+        string input = "";
+        cin >> input;
+
+        if(input == "exit"){
+            cout << "Thank you for playing!" << endl;
+            break;
+        }
+    }
+}
 
 void RunBattleships(){
     std::cout << "Battleships!" << std::endl;
-
+    DoLoop();
 }
