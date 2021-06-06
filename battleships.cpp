d487cded882664a7a4ae455997a14b1a63655c05
Linus Å <71346418+LynkanRex@users.noreply.github.com>
2021-06-06 22:14:53 +0200

    Reworking the Array into a C-style 2D array
    "Placing ships" works for demonstrating the functionality of the 2D Array and some basic input validation + placement validation (cannot place on H-row due to placement being for a 2-cell long ship)
    
    Further work if time allows / there is downtime during studies:
    
    * Further functionality like Ship class with width, rotation
    * Input/validation,
    * Check if any of the following cells (relative to ship-length) are occupied to avoid overlapping ships,
    * Random AI-placement(validating using same rules as player),
    * Targetting-matrix logic, Hit/Miss/Sink (On Hit, check if any remaining Ship cells contain the same name, like "S2", if no: send response Sink!
    
    * Also for AI: Random decision based on targeting matrix, unless something has been hit before, in that case hit around the area until it gets the response "Sink!"

--- battleships.cpp
+++ battleships.cpp
@@ -8,14 +8,16 @@
 using namespace std;
 
 const int playAreaSize = 8;
-using playAreaArrayRef = array<array<array<int,playAreaSize>, playAreaSize>, 2>;
+using playAreaArrayRef = int[8][8];
+using playAreaArrayRefAI = int[8][8];
 
 void DoBattleshipsLoop();
 
 // Playing Area Setup Declarations
-playAreaArrayRef InitializeGameplayArea();
+void InitializeGameplayAreaForPlayer(int(*a)[8][8]);
+void InitializeGameplayAreaForAI();
 bool ValidateShipPlacement(array<char, 2>, playAreaArrayRef);
-playAreaArrayRef PlaceShip(string position, playAreaArrayRef);
+void PlaceShip(string position, int(*a)[8][8]);
 
 
 // Gameplay Logic Declarations
@@ -24,91 +26,181 @@ bool ValidateInput(string);
 array<char, 2> SplitInput(string);
 array<int,2> ConvertInputSplitToValues(array<char,2>);
 
-playAreaArrayRef UpdateGameplayArea(playAreaArrayRef);
-void PrintGameplayArea(playAreaArrayRef);
+void UpdateGameplayArea(int[8][8],int[8][8]);
 
+void PrintGameplayArea(int (*a)[8][8], int (*b)[8][8]);
+void PlacementPrintGameplayArea(int [8][8]);
 
-void PrintGameplayArea(playAreaArrayRef currentPlayArea){
-    cout << "Printing game field" << endl;
+void PlacementPrintGameplayArea(int (*a)[8]){
+    for (int i = 0; i < 8; ++i) {
+        // TODO: Print ABCDEFGH
+        for (int j = 0; j < 8; ++j) {
+            if(a[i][j] == 32){
+                //TODO: Print 12345678
+                cout << " [" << ' ' << "] ";
+            }
+            else if(a[i][j] == 66)
+                cout << " [" << 'B' << "] ";
+            else{
+                cout << " [" << 'x' << "] ";
+            }
+        }
+        cout << endl;
+    }
 }
 
 
-bool ValidateShipPlacement(array<char, 2> splitPos, playAreaArrayRef currentPlayArea){
+
+void PrintGameplayArea(int (*a)[8], int (*b)[8]){
+
+    for (int i = 0; i < 8; ++i) {
+        // TODO: Print ABCDEFGH
+        for (int j = 0; j < 8; ++j) {
+           if(a[i][j] == 32){
+               //TODO: Print 12345678
+               cout << " [" << ' ' << "] ";
+           }
+           else if(a[i][j] == 66)
+               cout << " [" << 'B' << "] ";
+           else{
+               cout << " [" << 'x' << "] ";
+           }
+        }
+        cout << endl;
+    }
+    cout << "------------------------" << endl;
+    for (int i = 0; i < 8; ++i) {
+        // TODO: Print ABCDEFGH
+        for (int j = 0; j < 8; ++j) {
+            if(b[i][j] == 32){
+                //TODO: Print 12345678
+                cout << " [" << ' ' << "] ";
+            }
+            else if(b[i][j] == 66)
+                cout << " [" << 'B' << "] ";
+            else{
+                cout << " [" << 'x' << "] ";
+            }
+        }
+        cout << endl;
+    }
+
+}
+
+
+bool ValidateShipPlacement(array<char, 2> splitPos, int (*a)[8]){
     array<int,2> splitPosValues = ConvertInputSplitToValues(splitPos);
 
-    for(const auto& column : currentPlayArea)
-        for(const auto& row : column)
-            if(splitPosValues[0] == row.size()-1){
-                cout << "You cannot enter a position on the bottom line! Try again" << endl;
-                return 0;
+    cout << "Validating position" << splitPosValues[0] << splitPosValues[1] << endl;
 
+    if(splitPosValues[0] >= playAreaSize-1){
+        cout << "You cannot enter a position on the bottom line! Try again" << endl;
+        return 0;
+    }
+
+
+    for (int i = 0; i <8; ++i) {
+        for (int j = 0; j < 8; ++j) {
+            if(i == splitPosValues[0] && j == splitPosValues[1]){
+                if(a[splitPosValues[0]][splitPosValues[1]] != 32){
+                    cout << "The selected cell " << i<<j << " is not empty. Try again" << endl;
+                    return 0;
+                }
             }
-            else{
-                for(const auto& cell : row)
-                    if(cell != ' '){
-                        cout << "The selected cell is not empty. Try again" << endl;
-                        return 0;
-                    }
-            }
+        }
+    }
 
     return 1;
 }
 
-playAreaArrayRef PlaceShip(array<char, 2> position, playAreaArrayRef currentPlayArea){
-    playAreaArrayRef playArea = currentPlayArea;
+void PlaceShip(array<char, 2> position, int (*a)[8]){
 
     array<int,2> positionValues = ConvertInputSplitToValues(position);
 
+    cout << "Positioning ship at " << positionValues[0] << positionValues[1] << endl;
+    bool foundTargetCell = false;
 
+    for (int i = 0; i <8; ++i) {
+        for (int j = 0; j < 8; ++j) {
+            if(i == positionValues[0] && j == positionValues[1]){
+                a[positionValues[0]][positionValues[1]] = 66;
+                a[positionValues[0]+1][positionValues[1]] = 66;
+                foundTargetCell = true;
+                break;
+            }
+        }
+        if(foundTargetCell) break;
+    }
+    // TODO: Update contents of the specified cells and rows with something other than ' '
 
-
-
-    return playArea;
 }
 
 array<int,2> ConvertInputSplitToValues(array<char,2> splitInput){
 
     array<int, 2> inputValues;
 
+    splitInput[0] = tolower(splitInput[0]);
+
     switch (splitInput[0]) {
-        case 'A':
-            inputValues[0] = 1;
-        case 'B':
-            inputValues[0] = 2;
-        case 'C':
-            inputValues[0] = 3;
-        case 'D':
-            inputValues[0] = 4;
-        case 'E':
-            inputValues[0] = 5;
-        case 'F':
-            inputValues[0] = 6;
-        case 'G':
-            inputValues[0] = 7;
-        case 'H':
-            inputValues[0] = 8;
-        default:
+        case 'a':
+            inputValues[0] = 0;
+            break;
+        case 'b':
             inputValues[0] = 1;
             break;
-    }
-    switch (splitInput[1]) {
-        case 1:
-            inputValues[1] = 1;
-        case 2:
-            inputValues[1] = 2;
-        case 3:
-            inputValues[1] = 3;
-        case 4:
-            inputValues[1] = 4;
-        case 5:
-            inputValues[1] = 5;
-        case 6:
-            inputValues[1] = 6;
-        case 7:
-            inputValues[1] = 7;
-        case 8:
-            inputValues[1] = 8;
+        case 'c':
+            inputValues[0] = 2;
+            break;
+        case 'd':
+            inputValues[0] = 3;
+            break;
+        case 'e':
+            inputValues[0] = 4;
+            break;
+        case 'f':
+            inputValues[0] = 5;
+            break;
+        case 'g':
+            inputValues[0] = 6;
+            break;
+        case 'h':
+            inputValues[0] = 7;
+            break;
         default:
+            inputValues[1] = 0;
+            break;
+    }
+
+    int numberizedValue = splitInput[1]-48;
+
+
+    switch (numberizedValue) {
+        case 1:
+            inputValues[1] = 0;
+            break;
+        case 2:
+            inputValues[1] = 1;
+            break;
+        case 3:
+            inputValues[1] = 2;
+            break;
+        case 4:
+            inputValues[1] = 3;
+            break;
+        case 5:
+            inputValues[1] = 4;
+            break;
+        case 6:
+            inputValues[1] = 5;
+            break;
+        case 7:
+            inputValues[1] = 6;
+            break;
+        case 8:
+            inputValues[1] = 7;
+            break;
+        default:
+            inputValues[1] = 0;
             break;
     }
 
@@ -116,18 +208,23 @@ array<int,2> ConvertInputSplitToValues(array<char,2> splitInput){
 }
 
 
-playAreaArrayRef InitializeGameplayArea(){
-    playAreaArrayRef playArea;
-    bool placementDone = 0;
+void InitializeGameplayAreaForAI(){
+    playAreaArrayRefAI *playAreaAI;
 
-    PrintGameplayArea(playArea);
+    // TODO: Place AI's ships
 
+}
+
+
+void InitializeGameplayAreaForPlayer(int (*a)[8][8]){
     cout << "Placement phase, please enter a coordinate (like A1) where you want to place your ships:" << endl;
-    // TODO: Allow player to enter positions for their ships
 
+    bool placementDone = 0;
     int shipsLeft = 2;
 
     while(!placementDone){
+
+        PlacementPrintGameplayArea(*a);
         if(shipsLeft <= 0)
             break;
 
@@ -136,21 +233,19 @@ playAreaArrayRef InitializeGameplayArea(){
         string input = "";
         cin >> input;
 
+        // TODO: Add an If-tree for the AI randomized placements
+
         if(ValidateInput(input)){
             array<char,2> splitInput = SplitInput(input);
-            if(!ValidateShipPlacement(splitInput, playArea)){
-                // Do Nothing
-            }
-            else{
-                playArea = PlaceShip(splitInput, playArea);
+            if(ValidateShipPlacement(splitInput, *a))
+            {
+                PlaceShip(splitInput, *a);
                 shipsLeft--;
             }
         }
     }
 
     // TODO: Place AI's ships
-
-    return playArea;
 }
 
 
@@ -168,7 +263,6 @@ void DoBattleshipsTurn(string input){
 }
 
 array<char, 2> SplitInput(string input){
-    // TODO: Split input into a 2 index array, representing Column and Row to attack
     array<char, 2> splitInput;
 
     splitInput[0] = input[0];
@@ -178,7 +272,6 @@ array<char, 2> SplitInput(string input){
 }
 
 bool ValidateInput(string input){
-    // TODO: Check if input is 2 characters long, if the first entry is a character and the second entry is a number between 1-9
 
     if(input.length() <= 1 || input.length() >= 3)
     {
@@ -190,12 +283,16 @@ bool ValidateInput(string input){
         cout << "First character has to be a letter between A-H" << endl;
         return 0;
     }
-    if(!isdigit(input[1]) || (int)input[1] <= 0 || (int)input[1] > playAreaSize)
+    if(!isdigit(input[1]))
     {
         cout << input[1] << endl;
         cout << (int)input[1] << endl;
-        cout << "Second character has to be a a number between 1-8" << endl;
+        cout << "Second character has to be a a number" << endl;
         return 0;
+    }
+    if((int)input[1]-48 <= 0 || (int)input[1]-48 > playAreaSize){
+        cout << "Second number must be between 1 and 8" << endl;
+
     }
 
     return 1;
@@ -203,14 +300,35 @@ bool ValidateInput(string input){
 
 
 void DoBattleshipsLoop(){
-    playAreaArrayRef playArea = InitializeGameplayArea();
+    int playerAreaPlayer[8][8] = {{32,32,32,32,32,32,32,32},
+                                  {32,32,32,32,32,32,32,32},
+                                  {32,32,32,32,32,32,32,32},
+                                  {32,32,32,32,32,32,32,32},
+                                  {32,32,32,32,32,32,32,32},
+                                  {32,32,32,32,32,32,32,32},
+                                  {32,32,32,32,32,32,32,32},
+                                  {32,32,32,32,32,32,32,32}
+    };
+    int playAreaAI[8][8] = {{32,32,32,32,32,32,32,32},
+                            {32,32,32,32,32,32,32,32},
+                            {32,32,32,32,32,32,32,32},
+                            {32,32,32,32,32,32,32,32},
+                            {32,32,32,32,32,32,32,32},
+                            {32,32,32,32,32,32,32,32},
+                            {32,32,32,32,32,32,32,32},
+                            {32,32,32,32,32,32,32,32}
+    };
+
+    InitializeGameplayAreaForPlayer(&playerAreaPlayer);
 
     while(true){
+        PrintGameplayArea(playerAreaPlayer, playAreaAI);
+
         string input = "";
         cin >> input;
 
         if(input == "exit"){
-            cout << "Thank you for playing!" << endl;
+            cout << "Game over! Thank you for playing!" << endl;
             break;
         }
         else{
