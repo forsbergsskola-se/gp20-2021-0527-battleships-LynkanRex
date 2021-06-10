ad642431dd14d023e988c94e259a4ebecf6ec1f1
Linus Å <71346418+LynkanRex@users.noreply.github.com>
2021-06-10 10:42:32 +0200

    Randomized AI Placement of Ships

--- battleships.cpp
+++ battleships.cpp
@@ -5,6 +5,7 @@
 #include "battleships.h"
 #include <iostream>
 #include <array>
+#include <ctime>
 using namespace std;
 
 using playAreaArrayRef = int[8][8];
@@ -89,8 +90,8 @@ void PlacementPrintGameplayArea(Cell (*a)[8], Ship (playerShips)[5]){
 
 
 void PrintGameplayArea(Cell (*a)[8], Cell (*b)[8], Ship (playerShip)[5], Ship(AIShips)[5]){
-
     cout << "Enemy's Playfield;" << endl;
+
     for (int i = 0; i < 8; ++i) {
         // TODO: Print ABCDEFGH
         for (int j = 0; j < 8; ++j) {
@@ -98,18 +99,18 @@ void PrintGameplayArea(Cell (*a)[8], Cell (*b)[8], Ship (playerShip)[5], Ship(AI
             if(b[i][j].value == 32){
                 cout << " [" << "  " << "] ";
             }
-            else if(a[i][j].value == SmallCruiser1)
+            else if(b[i][j].value == SmallCruiser1)
                 cout << " [" << AIShips[0].shipType.nameSymbol << "] ";
-            else if(a[i][j].value == SmallCruiser2)
+            else if(b[i][j].value == SmallCruiser2)
                 cout << " [" << AIShips[1].shipType.nameSymbol << "] ";
-            else if(a[i][j].value == Corsair)
+            else if(b[i][j].value == Corsair)
                 cout << " [" << AIShips[2].shipType.nameSymbol << "] ";
-            else if(a[i][j].value == Battleship)
+            else if(b[i][j].value == Battleship)
                 cout << " [" << AIShips[3].shipType.nameSymbol << "] ";
-            else if(a[i][j].value == Carrier)
+            else if(b[i][j].value == Carrier)
                 cout << " [" << AIShips[4].shipType.nameSymbol << "] ";
             else{
-                cout << " [" << 'x' << "] ";
+                cout << " [" << "x " << "] ";
             }
         }
         cout << endl;
@@ -134,7 +135,7 @@ void PrintGameplayArea(Cell (*a)[8], Cell (*b)[8], Ship (playerShip)[5], Ship(AI
             else if(a[i][j].value == Carrier)
                 cout << " [" << playerShip[4].shipType.nameSymbol << "] ";
             else{
-                cout << " [" << 'x' << "] ";
+                cout << " [" << "x " << "] ";
             }
         }
         cout << endl;
@@ -143,12 +144,13 @@ void PrintGameplayArea(Cell (*a)[8], Cell (*b)[8], Ship (playerShip)[5], Ship(AI
 }
 
 
-bool ValidateShipPlacement(array<char, 3> splitPos, Cell (*a)[8], Ship shipToPlace){
+bool ValidateShipPlacement(array<char, 3> splitPos, Cell (*a)[8], Ship shipToPlace, bool playerTurn){
     array<int,3> splitPosValues = ConvertInputSplitToValues(splitPos);
 
     if(splitPosValues[2] == 0){
         if(splitPosValues[0]+shipToPlace.shipType.size > playAreaSize){
-            cout << "Placing a ship here, with this rotation would overflow downwards" << endl;
+            if(playerTurn)
+                cout << "Placing a ship here, with this rotation would overflow downwards" << endl;
             return 0;
         }
 
@@ -157,13 +159,15 @@ bool ValidateShipPlacement(array<char, 3> splitPos, Cell (*a)[8], Ship shipToPla
             for (int j = 0; j < playAreaSize; ++j) {
                 if(i == splitPosValues[0] && j == splitPosValues[1]){
                     if(a[splitPosValues[0]][splitPosValues[1]].value != 32){
-                        cout << "The selected cell is not empty. Try again" << endl;
+                        if(playerTurn)
+                            cout << "The selected cell is not empty. Try again" << endl;
                         return 0;
                     }
                     else{
                         for (int k = 1; k < shipToPlace.shipType.size; ++k) {
                             if(a[splitPosValues[0]+k][splitPosValues[1]].value != 32){
-                                cout << "Non-empty cells downards after your selection detected. Try something else" << endl;
+                                if(playerTurn)
+                                    cout << "Non-empty cells downards after your selection detected. Try something else" << endl;
                                 return 0;
                             }
                         }
@@ -174,7 +178,8 @@ bool ValidateShipPlacement(array<char, 3> splitPos, Cell (*a)[8], Ship shipToPla
     }
     else if(splitPosValues[2] == 1){
         if(splitPosValues[1]+shipToPlace.shipType.size > playAreaSize){
-            cout << "Placing a ship here, with this rotation would overflow to the right" << endl;
+            if(playerTurn)
+                cout << "Placing a ship here, with this rotation would overflow to the right" << endl;
             return 0;
         }
 
@@ -183,13 +188,15 @@ bool ValidateShipPlacement(array<char, 3> splitPos, Cell (*a)[8], Ship shipToPla
             for (int j = 0; j < playAreaSize; ++j) {
                 if(i == splitPosValues[0] && j == splitPosValues[1]){
                     if(a[splitPosValues[0]][splitPosValues[1]].value != 32){
-                        cout << "The selected cell is not empty. Try again" << endl;
+                        if(playerTurn)
+                            cout << "The selected cell is not empty. Try again" << endl;
                         return 0;
                     }
                     else{
                         for (int k = 1; k < shipToPlace.shipType.size; ++k) {
                             if(a[splitPosValues[0]][splitPosValues[1]+k].value != 32){
-                                cout << "Non-empty cells to the right of your selection detected. Try something else" << endl;
+                                if(playerTurn)
+                                    cout << "Non-empty cells to the right of your selection detected. Try something else" << endl;
                                 return 0;
                             }
                         }
@@ -397,9 +404,108 @@ array<int,3> ConvertInputSplitToValues(array<char,3> splitInput){
 }
 
 
-void InitializeGameplayAreaForAI(Cell (*a)[8][8], Ship (aiShips)[5]){
+string RandomizedAIPlacement(){
+    string newString = "000";
+
+    newString[0] = (rand() % 8) +1;
+    newString[1] = (rand() % 8) +1;
+    newString[2] = (int)(rand() % 2);
+
+
+    switch (newString[0]) {
+        case 0:
+            newString[0] = 'A';
+            break;
+        case 1:
+            newString[0] = 'B';
+            break;
+        case 2:
+            newString[0] = 'C';
+            break;
+        case 3:
+            newString[0] = 'D';
+            break;
+        case 4:
+            newString[0] = 'E';
+            break;
+        case 5:
+            newString[0] = 'F';
+            break;
+        case 6:
+            newString[0] = 'G';
+            break;
+        case 7:
+            newString[0] = 'H';
+            break;
+        default:
+            newString[0] = 'A';
+            break;
+    }
+    switch (newString[1]) {
+        case 0:
+            newString[1] = '1';
+            break;
+        case 1:
+            newString[1] = '2';
+            break;
+        case 2:
+            newString[1] = '3';
+            break;
+        case 3:
+            newString[1] = '4';
+            break;
+        case 4:
+            newString[1] = '5';
+            break;
+        case 5:
+            newString[1] = '6';
+            break;
+        case 6:
+            newString[1] = '7';
+            break;
+        case 7:
+            newString[1] = '8';
+            break;
+        default:
+            newString[1] = '1';
+            break;
+    }
+    switch (newString[2]) {
+        case 0:
+            newString[2] = 'D';
+            break;
+        case 1:
+            newString[2] = 'R';
+            break;
+        default:
+            newString[2] = 'D';
+            break;
+    }
+
+    return newString;
+}
+
+void InitializeGameplayAreaForAI(Cell (*b)[8][8], Ship (aiShips)[5]){
+    bool placementDone = 0;
     cout << "Enemy is placing their ships..." << endl;
 
+    int placedShips = 0;
+    int shipsToPlace = 5;
+
+    while(!placementDone){
+        if(placedShips >= shipsToPlace)
+            break;
+
+        string AIInput = RandomizedAIPlacement();
+
+        if(ValidateInput(AIInput)){
+            array<char, 3> AIPlayerPlacementInput = SplitInput(AIInput);
+            if(ValidateShipPlacement(AIPlayerPlacementInput, *b, aiShips[placedShips], false)){
+                PlaceShip(AIPlayerPlacementInput, *b, aiShips[placedShips]);
+                placedShips++;
+            }
+        }
+    }
     // TODO: Place AI's ships
 }
 
@@ -424,7 +530,7 @@ void InitializeGameplayAreaForPlayer(Cell (*a)[8][8], Ship (playerShips)[5]){
 
         if(ValidateInput(input)){
             array<char,3> splitInput = SplitInput(input);
-            if(ValidateShipPlacement(splitInput, *a, playerShips[placedShips]))
+            if(ValidateShipPlacement(splitInput, *a, playerShips[placedShips], true))
             {
                 PlaceShip(splitInput, *a, playerShips[placedShips]);
                 placedShips++;
@@ -523,7 +629,7 @@ Ship* SetupShips(Ship ships[5]){
 void DoBattleshipsLoop(){
 
     Cell playerAreaPlayer[playAreaSize][playAreaSize];
-    Cell playAreaAI[playAreaSize][playAreaSize];
+    Cell playerAreaAI[playAreaSize][playAreaSize];
 
     Ship playerShips[5];
     Ship AIShips[5];
@@ -532,10 +638,10 @@ void DoBattleshipsLoop(){
     SetupShips(AIShips);
 
     InitializeGameplayAreaForPlayer(&playerAreaPlayer, playerShips);
-    InitializeGameplayAreaForAI(&playAreaAI, AIShips);
+    InitializeGameplayAreaForAI(&playerAreaAI, AIShips);
 
     while(true){
-        PrintGameplayArea(playerAreaPlayer, playAreaAI, playerShips, AIShips);
+        PrintGameplayArea(playerAreaPlayer, playerAreaAI, playerShips, AIShips);
 
         // TODO: Toggle back and forth between player and AI turn
 
@@ -555,6 +661,7 @@ void DoBattleshipsLoop(){
 
 
 void RunBattleships(){
+    srand(time(NULL));
     std::cout << "Battleships!" << std::endl;
     DoBattleshipsLoop();
 }
